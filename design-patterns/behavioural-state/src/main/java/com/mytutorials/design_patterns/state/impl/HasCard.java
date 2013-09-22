b474802327f8b570d7d82c78bebd02d94aa0f15e
Javaid <Javaid@Javaid-6935>
2013-09-22 14:09:34 +0400

    state design pattern added

---
+++ design-patterns/behavioural-state/src/main/java/com/mytutorials/design_patterns/state/impl/HasCard.java
@@ -0,0 +1,59 @@
+package com.mytutorials.design_patterns.state.impl;
+
+import com.mytutorials.design_patterns.state.ATMState;
+import com.mytutorials.design_patterns.state.context.ATMMachine;
+
+public class HasCard implements ATMState {
+
+	private ATMMachine atmMachine;
+
+	public HasCard(ATMMachine newATMMachine) {
+
+		atmMachine = newATMMachine;
+
+	}
+
+	public void insertCard() {
+
+		System.out.println("You can only insert one card at a time");
+
+	}
+
+	public void ejectCard() {
+
+		System.out.println("Your card is ejected");
+
+		atmMachine.setATMState(atmMachine.getNoCardState());
+
+	}
+
+	public void requestCash(int cashToWithdraw) {
+
+		System.out.println("You have not entered your PIN");
+
+	}
+
+	public void insertPin(int pinEntered) {
+
+		if (pinEntered == 1234) {
+
+			System.out.println("You entered the correct PIN");
+
+			atmMachine.setCorrectPinEntered(true);
+
+			atmMachine.setATMState(atmMachine.getHasPin());
+
+		} else {
+
+			System.out.println("You entered the wrong PIN");
+
+			atmMachine.setCorrectPinEntered(false);
+
+			System.out.println("Your card is ejected");
+
+			atmMachine.setATMState(atmMachine.getNoCardState());
+
+		}
+	}
+
+}

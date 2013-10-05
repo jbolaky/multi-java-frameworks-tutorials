b9c6e009bde1f068d9bb4e8dc540ee4e093504e6
Javaid <Javaid@Javaid-6935.zte.com.cn>
2013-10-05 22:49:05 +0400

    prototype design pattern added

---
+++ design-patterns/object-creational-prototype/src/main/java/com/mytutorials/design_patterns/prototype/Animal.java
@@ -0,0 +1,17 @@
+package com.mytutorials.design_patterns.prototype;
+
+//By making this class cloneable you are telling Java
+
+//that it is ok to copy instances of this class
+
+//These instance copies have different results when
+
+//System.identityHashCode(System.identityHashCode(bike))
+
+//is called
+
+public interface Animal extends Cloneable {
+
+	public Animal makeCopy();
+
+}

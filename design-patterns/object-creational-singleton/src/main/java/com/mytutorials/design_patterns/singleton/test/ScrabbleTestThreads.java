6b0162046bfc363cbef2069129b7a189e12941a5
Javaid <Javaid@Javaid-6935.zte.com.cn>
2013-10-06 00:25:05 +0400

    singleton design pattern added

---
+++ design-patterns/object-creational-singleton/src/main/java/com/mytutorials/design_patterns/singleton/test/ScrabbleTestThreads.java
@@ -0,0 +1,23 @@
+package com.mytutorials.design_patterns.singleton.test;
+
+import com.mytutorials.design_patterns.singleton.thread.GetTheTiles;
+
+public class ScrabbleTestThreads {
+	public static void main(String[] args) {
+
+		// Create a new Thread created using the Runnable interface
+
+		// Execute the code in run after 10 seconds
+
+		Runnable getTiles = new GetTheTiles();
+
+		Runnable getTilesAgain = new GetTheTiles();
+
+		// Call for the code in the method run to execute
+
+		new Thread(getTiles).start();
+
+		new Thread(getTilesAgain).start();
+
+	}
+}
